1a8f8b2cb5669ce8302cbdc2b311da4fbdb513fa
Ronny Polley <ronnypolley@googlemail.com>
2016-07-03 21:28:22 +0200

    added header licence to source files

--- bin/babun-docker.sh
+++ bin/babun-docker.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
-# Fix for Docker and Docker Toolbox in Babun
-if [[ -z "$docker_bin" ]] ; then
-	docker_bin=$(which docker) ;
-fi
+#*******************************************************************************
+# Copyright (c) 2016 Ronny Polley
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Ronny Polley - main contributor
+#******************************************************************************/
+
+
+# get the location of the docker binary
+docker_bin=$(which docker) ;
+
 # we just prefix the docker command with the console.exe from winpty
 function docker {
 	winpty $docker_bin $@
--- setup.sh
+++ setup.sh
@@ -1,88 +1,98 @@
 #! /bin/bash
 
+#*******************************************************************************
+# Copyright (c) 2016 Ronny Polley
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Ronny Polley - main contributor
+#******************************************************************************/
+
+
 # winpty version
-babun_docker_winpty_version="0.4.0"
+winpty_version="0.4.0"
 
-# winpty base URL
-babun_docker_winpty_base_url="https://github.com/rprichard/winpty/releases/download/${babun_docker_winpty_version}/"
+# winpty name
+winpty_name="winpty-${winpty_version}-cygwin-2.5.2-ia32"
 
-# Specific file name, separated to allow unzipping it later
-babun_docker_winpty_only_name="winpty-${babun_docker_winpty_version}-cygwin-2.5.2-ia32"
+# Specific file name
+winpty_filename="${winpty_name}.tar.gz"
 
-# Specific file extension
-babun_docker_winpty_ext=".tar.gz"
-
-# Complete filename
-babun_docker_winpty_filename="$babun_docker_winpty_only_name$babun_docker_winpty_ext"
+# winpty URL
+winpty_url="https://github.com/rprichard/winpty/releases/download/${winpty_version}/${winpty_filename}"
 
 # babun-docker repo
 babun_docker_repo='https://github.com/ronnypolley/babun-docker4windows.git'
 
-# feedback format string
-babun_docker_feedback='-- babun-docker:'
+# put current folder on the stack
+pushd .
 
-babun_docker_OLD_PWD=$(pwd)
-# Set up winpty
-# Where to download winpty from
-babun_docker_winpty_url="$babun_docker_winpty_base_url$babun_docker_winpty_filename"
 # Directory in where to put Winpty
-babun_docker_winpty_dir="$HOME/.winpty"
-# Create a local .winpty directory under your home
-if [[ ! -d $babun_docker_winpty_dir ]] ; then
-   mkdir -p $babun_docker_winpty_dir ;
+winpty_dir="$HOME/.winpty"
+
+# Create the local .winpty directory
+if [[ ! -d $winpty_dir ]] ; then
+   mkdir -p $winpty_dir ;
 fi
+
 # Enter the .winpty directory to download winpty
-cd $babun_docker_winpty_dir
-if [[ ! -f $babun_docker_winpty_filename ]] ; then
+cd $winpty_dir
+if [[ ! -f winpty.exe ]] ; then
    # Remove old files
-   cd ..
-   rm -rf $babun_docker_winpty_dir
-   mkdir -p $babun_docker_winpty_dir
-   cd $babun_docker_winpty_dir
+   rm -rf ./*
    # Download winpty
-   wget $babun_docker_winpty_url
-   # Untar the downloaded file
-   tar xf $babun_docker_winpty_filename
+   wget -O - $winpty_url | tar xvz 
    # Move the tar contents to the current directory
-   mv $babun_docker_winpty_only_name/bin/* ./
+   mv $winpty_name/bin/* ./
    # Remove untar-ed directory
-   rm -rf $babun_docker_winpty_only_name
-   # Make console executable
-   chmod 777 $babun_docker_winpty_dir/*
+   rm -rf $winpty_name
+   # Make winpty executable
+   chmod 777 $winpty_dir/*
+   # write version to directory
+   echo $winpty_version > version
 fi
-export PATH="$PATH:$babun_docker_winpty_dir"
+# add winpty path to the PATH variable
+export PATH="$PATH:$winpty_dir"
 
 
-# Set up babun-docker
-# Directory in where to put babun-docker
-babun_docker_repo_dir="$HOME/.babun-docker4windows"
-if [[ ! -d $babun_docker_repo_dir ]] ; then
-   git clone $babun_docker_repo $babun_docker_repo_dir
+# Set up babun-docker4windows
+# Directory in where to put babun-docker4windows
+babun_docker4windows_dir="$HOME/.babun-docker4windows"
+
+if [[ ! -d $babun_docker4windows_dir ]] ; then
+   git clone $babun_docker_repo $babun_docker4windows_dir
 fi
-cd $babun_docker_repo_dir
+cd $babun_docker4windows_dir
 source 'bin/babun-docker.sh'
 
+babun_docker_setup_str="source '$babun_docker4windows_dir/setup.sh'"
 
-# Set up setup on start for Bash
-babun_docker_setup_str="source '$babun_docker_repo_dir/setup.sh'"
-if [[ -z $(grep "$babun_docker_setup_str" $HOME/.bashrc) ]] ; then
-  echo $babun_docker_setup_str >> $HOME/.bashrc ;
+# Set up setup on start for bash if needed
+if [[ -f $HOME/.bashrc ]] ; then
+	if [[ -z $(grep "$babun_docker_setup_str" $HOME/.bashrc) ]] ; then
+	  echo $babun_docker_setup_str >> $HOME/.bashrc ;
+	fi
 fi
 
 # Set up setup on start for Zsh
-babun_docker_setup_str="source '$babun_docker_repo_dir/setup.sh'"
-if [[ -z $(grep "$babun_docker_setup_str" $HOME/.zshrc) ]] ; then
-  echo $babun_docker_setup_str >> $HOME/.zshrc ;
+if [[ -f $HOME/.zshrc ]] ; then
+  if [[ -z $(grep "$babun_docker_setup_str" $HOME/.zshrc) ]] ; then
+		echo $babun_docker_setup_str >> $HOME/.zshrc ;
+	fi
 fi
 
 # Setup update
-function babun-docker-update {
-  babun_docker_update_old_pwd=$(pwd)
-  echo "$babun_docker_feedback Updating babun-docker"
-  cd $babun_docker_repo_dir
-  git pull
-  source ./setup.sh
-  cd $babun_docker_update_old_pwd
+function babun-docker4windows-update {
+	# save working directory
+	pushd $(pwd)
+	echo "Updating babun-docker4windows"
+	cd $babun_docker4windows_dir
+	git pull
+	source ./setup.sh
+	popd
 }
 
-cd $babun_docker_OLD_PWD
+popd
